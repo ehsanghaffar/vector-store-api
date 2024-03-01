503645cab7d045e883e238a87aee307e6160b28f
ehsanghaffarii <ghafari.5000@gmail.com>
2024-03-01 13:10:10 +0330

    Add middlewares and routers to FastAPI application

---
+++ app/core/bootstrap/middlewares.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from core.config import Settings, get_settings
+from core.middlewares.logging import LoggingMiddleware
+from core.middlewares.request_id import RequestIdMiddleware
+
+
+def init_middlewares(app_: FastAPI) -> None:
+    settings: Settings = get_settings()
+
+    app_.add_middleware(LoggingMiddleware)
+    app_.add_middleware(RequestIdMiddleware)
+
+    # CORSMiddleware
+    app_.add_middleware(
+        CORSMiddleware,
+        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
