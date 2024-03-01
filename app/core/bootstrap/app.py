c392c55be68f8259f7fb198856f5e54962d2f859
ehsanghaffarii <ghafari.5000@gmail.com>
2024-03-01 13:09:52 +0330

    Add exception handlers for custom exceptions and request validation errors

---
+++ app/core/bootstrap/app.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+
+from core.config import Settings, get_settings
+
+from .middlewares import init_middlewares
+from .routers import init_routers
+from .handlers import init_handlers
+
+
+def create_app() -> FastAPI:
+    settings: Settings = get_settings()
+
+    app_ = FastAPI(
+        title=settings.PROJECT_TITLE,
+        description=settings.PROJECT_DESCRIPTION,
+        version=settings.PROJECT_VERSION,
+        docs_url=None if settings.ENV == "prod" else "/docs",
+        redoc_url=None if settings.ENV == "prod" else "/redoc",
+    )
+
+    # Initializing required dependencies
+    init_handlers(app_=app_)
+    init_middlewares(app_=app_)
+    init_routers(app_=app_)
+
+    return app_
