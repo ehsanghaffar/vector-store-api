63da12f89e38f61705c17a62bac1aeafa476bb08
ehsanghaffarii <ghafari.5000@gmail.com>
2024-03-03 05:07:49 +0330

    Add .env.example, .gitignore, and core module, include embed router

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter
-from core.config import get_settings
+from app.core.config import get_settings
 
-# from .register.routes import register_router as register_v1_router
+from .embed.routes import embed_router as embed_v1_router
 
 settings = get_settings()
 
 router = APIRouter()
-# router.include_router(register_v1_router, prefix=settings.API_V1_PREFIX + "/register", tags=["Registration"])
+
+router.include_router(embed_v1_router, prefix=settings.API_V1_PREFIX + "/embed", tags=["Embedding"])
 
 __all__ = ["router"]
