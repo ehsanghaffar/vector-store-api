1850524c294cedf1f2897116b5096eab88727dfa
ehsanghaffarii <ghafari.5000@gmail.com>
2024-04-25 10:32:12 +0330

    Add local file embedding functionality to service

--- app/api/v1/on_disk/schemas/requests.py
+++ app/api/v1/on_disk/schemas/requests.py
@@ -1,5 +1,6 @@
 from enum import Enum
 
+from fastapi import UploadFile
 from pydantic import BaseModel, Field
 
 
@@ -7,4 +8,9 @@ from pydantic import BaseModel, Field
 
 class EmbedOnlineFileRequest(BaseModel):
     file_url: str = Field(..., description="File URL")
+    collection_name: str = Field(..., description="Name of collection")
+
+
+class EmbedOfflineFileRequest(BaseModel):
+    file: UploadFile = Field(..., description="File")
     collection_name: str = Field(..., description="Name of collection")
--- app/api/v1/on_disk/service.py
+++ app/api/v1/on_disk/service.py
@@ -1,7 +1,6 @@
-import logging
 import os
-import shutil
 from typing import Dict, List
+from fastapi import File
 from langchain_community.document_loaders import (
     CSVLoader,
     EverNoteLoader,
@@ -19,7 +18,7 @@ from langchain.schema import Document
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from app.api.v1.on_disk.schemas.requests import EmbedOnlineFileRequest
 from app.core.config import get_settings
-from app.core.utils import download_file
+from app.core.utils import download_file, upload_file
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores.chroma import Chroma
 from chromadb import ClientAPI
@@ -44,6 +43,19 @@ LOADER_MAPPING = {
     ".txt": (TextLoader, {"encoding": "utf8"}),
 }
 
+
+async def load_documents_from_file(file: File):
+    file_path = upload_file(file=file, save_path="tmp")
+    ext = "." + file_path.rsplit(".", 1)[-1]
+    if ext in LOADER_MAPPING:
+        loader_class, loader_args = LOADER_MAPPING[ext]
+        loader = loader_class(file_path, **loader_args)
+        data = loader.load()
+        return data, file_path
+
+    raise ValueError(f"Unsupported file extension '{ext}'")
+
+
 def load_documents_from_url(file_url: str, tmp_path: str = "tmp"):
 
     file_path = download_file(url=file_url, save_path=tmp_path)
@@ -55,8 +67,6 @@ def load_documents_from_url(file_url: str, tmp_path: str = "tmp"):
         return loader.load()
 
     raise ValueError(f"Unsupported file extension '{ext}'")
-
-
 
 
 def split_text(documents: list[Document]):
@@ -71,30 +81,48 @@ def split_text(documents: list[Document]):
 
     return chunks
 
+
 embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
 
 
 def save_to_chroma(chunks: list[Document], chroma_path: str, collection: str):
 
     db = Chroma.from_documents(
-        documents=chunks, embedding=embeddings, persist_directory=chroma_path, collection_name=collection
+        documents=chunks,
+        embedding=embeddings,
+        persist_directory=chroma_path,
+        collection_name=collection,
     )
     db.persist()
     print(f"Saved {len(chunks)} chunks to {CHROMA_PATH}.")
-
 
 
 class EmbeddingService:
     def __init__(self):
         self.settings = get_settings()
 
+    async def embedLocal(self, file: File, collection: str):
+        loaded_file, file_path = await load_documents_from_file(file=file)
+        os.remove(file_path)
+        documents = split_text(loaded_file)
+        save_to_chroma(
+            chunks=documents,
+            chroma_path=self.settings.CHROMA_PATH,
+            collection=collection,
+        )
+
     async def embedRemote(self, embed_request: EmbedOnlineFileRequest):
         loaded_file = load_documents_from_url(file_url=embed_request.file_url)
         documents = split_text(loaded_file)
-        save_to_chroma(chunks=documents, chroma_path=self.settings.CHROMA_PATH, collection=embed_request.collection_name)
+        save_to_chroma(
+            chunks=documents,
+            chroma_path=self.settings.CHROMA_PATH,
+            collection=embed_request.collection_name,
+        )
 
-
-    def add_to_collection(self, persistent_client: ClientAPI, collection_name: str, items: Dict[str, str]):
+    def add_to_collection(
+        self, persistent_client: ClientAPI, collection_name: str, items: Dict[str, str]
+    ):
         collection = persistent_client.get_or_create_collection(collection_name)
         collection.add(ids=list(items.keys()), documents=list(items.values()))
 
@@ -102,6 +130,6 @@ class EmbeddingService:
         langchain_chroma = Chroma(
             client=persistent_client,
             collection_name=collection_name,
-            embedding_function=embeddings
+            embedding_function=embeddings,
         )
         return langchain_chroma._collection.get()
